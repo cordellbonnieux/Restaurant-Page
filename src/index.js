de740cde898df513844f0a7a70663b416dabef8d
cordellbonnieux <cordellbonnieux@gmail.com>
2020-09-17 12:58:32 -0700

    Completed the menu.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import {start} from "./start.js";
 import {tabs} from "./tabsy.js";
 import {about} from "./about.js";
-import {starters} from "./menu.js";
+import {menu} from "./menu.js";
 import {top} from "./top.js";
 // load page
 const content = document.getElementById('content');
@@ -9,9 +9,7 @@ start(content);
 tabs();
 about();
 top();
-//menu
-//starters();
-
+menu();
 
 // add images (make a module later)
 import background from "./img/wallpaper.png";
@@ -23,5 +21,77 @@ mascot.src = donDon;
 mascot.setAttribute("id", "donMascot");
 const headerLeft = document.getElementById("headerLeft");
 headerLeft.appendChild(mascot);
+// add menu item images
+import starter1 from "./img/menu/starter1.jpg";
+import starter2 from "./img/menu/starter2.jpg";
+import starter3 from "./img/menu/starter3.jpg";
+//
+import sandwich1 from "./img/menu/sandwich1.jpg";
+import sandwich2 from "./img/menu/sandwich2.jpg";
+import sandwich3 from "./img/menu/sandwich3.jpg";
+//
+import bowl1 from "./img/menu/bowl1.jpg";
+import bowl2 from "./img/menu/bowl2.jpg";
+import bowl3 from "./img/menu/bowl3.jpg";
+//
+const starter1Id = document.getElementById("starter1");
+const starter2Id = document.getElementById("starter2");
+const starter3Id = document.getElementById("starter3");
+//
+const sandwich1Id = document.getElementById("sandwich1");
+const sandwich2Id = document.getElementById("sandwich2");
+const sandwich3Id = document.getElementById("sandwich3");
+//
+const bowl1Id = document.getElementById("bowl1");
+const bowl2Id = document.getElementById("bowl2");
+const bowl3Id = document.getElementById("bowl3");
+// starters
+let starter1Img = new Image();
+    starter1Img.src = starter1;
+    starter1Img.setAttribute("id", "starter1Img");
+    starter1Img.setAttribute("class", "menuImg");
+    starter1Id.prepend(starter1Img);
+let starter2Img = new Image();
+    starter2Img.src = starter2;
+    starter2Img.setAttribute("id", "starter2Img");
+    starter2Img.setAttribute("class", "menuImg");
+    starter2Id.prepend(starter2Img);
+let starter3Img = new Image();
+    starter3Img.src = starter3;
+    starter3Img.setAttribute("id", "starter3Img");
+    starter3Img.setAttribute("class", "menuImg");
+    starter3Id.prepend(starter3Img);
+// sandwiches
+let sandwich1Img = new Image();
+    sandwich1Img.src = sandwich1;
+    sandwich1Img.setAttribute("id", "sandwich1Img");
+    sandwich1Img.setAttribute("class", "menuImg");
+    sandwich1Id.prepend(sandwich1Img);
+let sandwich2Img = new Image();
+    sandwich2Img.src = sandwich2;
+    sandwich2Img.setAttribute("id", "sandwich2Img");
+    sandwich2Img.setAttribute("class", "menuImg");
+    sandwich2Id.prepend(sandwich2Img);
+let sandwich3Img = new Image();
+    sandwich3Img.src = sandwich3;
+    sandwich3Img.setAttribute("id", "sandwich3Img");
+    sandwich3Img.setAttribute("class", "menuImg");
+    sandwich3Id.prepend(sandwich3Img);
+// bowls
+let bowl1Img = new Image();
+    bowl1Img.src = bowl1;
+    bowl1Img.setAttribute("id", "bowl1Img");
+    bowl1Img.setAttribute("class", "menuImg");
+    bowl1Id.prepend(bowl1Img);
+let bowl2Img = new Image();
+    bowl2Img.src = bowl2;
+    bowl2Img.setAttribute("id", "bowl2Img");
+    bowl2Img.setAttribute("class", "menuImg");
+    bowl2Id.prepend(bowl2Img);
+let bowl3Img = new Image();
+    bowl3Img.src = bowl3;
+    bowl3Img.setAttribute("id", "bowl3Img");
+    bowl3Img.setAttribute("class", "menuImg");
+    bowl3Id.prepend(bowl3Img);
 //css
 import css from "./style.css";
--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,18 @@
-let menu = document.getElementById("menu");
-export function starters(){
+export function menu(){
+    let menuContainer = document.getElementById("menu");
+    //starters Container
+    let startersContainer = document.createElement("div");
+        startersContainer.setAttribute("id", "startersContainer");
+        menuContainer.appendChild(startersContainer);
+        // starters title
+    let startersTitle = document.createElement("h2");
+        startersTitle.textContent = "Starters";
+        startersContainer.appendChild(startersTitle);
     //starters
     let starters = document.createElement("div");
         starters.setAttribute("id", "starters");
         starters.setAttribute("class", "menuSection");
-        menu.appendChild(starters);
+        startersContainer.appendChild(starters);
         // starter 1
         let starter1 = document.createElement("div");
             starter1.setAttribute("class", "menuItem");
@@ -12,7 +20,7 @@ export function starters(){
             let starter1Title = document.createElement("h3");
                 starter1Title.textContent = "Starter 1"
             let starter1Price = document.createElement("span");
-                starter1Price.textContent = "8";
+                starter1Price.textContent = "12";
             let starter1Text = document.createElement("p");
                 starter1Text.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce iaculis tristique diam sed mattis. Nunc scelerisque mollis felis ac molestie.";
             starters.appendChild(starter1);
@@ -40,12 +48,122 @@ export function starters(){
             let starter3Title = document.createElement("h3");
                 starter3Title.textContent = "Starter 3"
             let starter3Price = document.createElement("span");
-                starter3Price.textContent = "8";
+                starter3Price.textContent = "7";
             let starter3Text = document.createElement("p");
                 starter3Text.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce iaculis tristique diam sed mattis. Nunc scelerisque mollis felis ac molestie.";
             starters.appendChild(starter3);
             starter3.appendChild(starter3Title);
             starter3.appendChild(starter3Price);
             starter3.appendChild(starter3Text);
+    //sandwiches Container
+    let sandwichesContainer = document.createElement("div");
+        sandwichesContainer.setAttribute("id", "sandwichesContainer");
+        menuContainer.appendChild(sandwichesContainer);
+    // sandwiches title
+    let sandwichesTitle = document.createElement("h2");
+        sandwichesTitle.textContent = "Sandwiches";
+        sandwichesContainer.appendChild(sandwichesTitle);
+    // sandwiches
+    let sandwiches = document.createElement("div");
+        sandwiches.setAttribute("id", "sandwiches");
+        sandwiches.setAttribute("class", "menuSection");
+        sandwichesContainer.appendChild(sandwiches);
+        // sandwich 1
+        let sandwich1 = document.createElement("div");
+            sandwich1.setAttribute("class", "menuItem");
+            sandwich1.setAttribute("id", "sandwich1");
+            let sandwich1Title = document.createElement("h3");
+                sandwich1Title.textContent = "sandwich 1"
+            let sandwich1Price = document.createElement("span");
+                sandwich1Price.textContent = "8";
+            let sandwich1Text = document.createElement("p");
+                sandwich1Text.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce iaculis tristique diam sed mattis. Nunc scelerisque mollis felis ac molestie.";
+            sandwiches.appendChild(sandwich1);
+            sandwich1.appendChild(sandwich1Title);
+            sandwich1.appendChild(sandwich1Price);
+            sandwich1.appendChild(sandwich1Text);
+        // sandwich 2
+        let sandwich2 = document.createElement("div");
+            sandwich2.setAttribute("class", "menuItem");
+            sandwich2.setAttribute("id", "sandwich2");
+            let sandwich2Title = document.createElement("h3");
+                sandwich2Title.textContent = "sandwich 2"
+            let sandwich2Price = document.createElement("span");
+                sandwich2Price.textContent = "8";
+            let sandwich2Text = document.createElement("p");
+                sandwich2Text.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce iaculis tristique diam sed mattis. Nunc scelerisque mollis felis ac molestie.";
+            sandwiches.appendChild(sandwich2);
+            sandwich2.appendChild(sandwich2Title);
+            sandwich2.appendChild(sandwich2Price);
+            sandwich2.appendChild(sandwich2Text);
+        // sandwich 3
+        let sandwich3 = document.createElement("div");
+            sandwich3.setAttribute("class", "menuItem");
+            sandwich3.setAttribute("id", "sandwich3");
+            let sandwich3Title = document.createElement("h3");
+                sandwich3Title.textContent = "sandwich 3"
+            let sandwich3Price = document.createElement("span");
+                sandwich3Price.textContent = "8";
+            let sandwich3Text = document.createElement("p");
+                sandwich3Text.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce iaculis tristique diam sed mattis. Nunc scelerisque mollis felis ac molestie.";
+            sandwiches.appendChild(sandwich3);
+            sandwich3.appendChild(sandwich3Title);
+            sandwich3.appendChild(sandwich3Price);
+            sandwich3.appendChild(sandwich3Text);
+    //bowls Container
+    let bowlsContainer = document.createElement("div");
+        bowlsContainer.setAttribute("id", "bowlsContainer");
+        menuContainer.appendChild(bowlsContainer);
+        // starters title
+    let bowlsTitle = document.createElement("h2");
+        bowlsTitle.textContent = "Bowls";
+        bowlsContainer.appendChild(bowlsTitle);
+    //starters
+    let bowls = document.createElement("div");
+        bowls.setAttribute("id", "bowls");
+        bowls.setAttribute("class", "menuSection");
+        bowlsContainer.appendChild(bowls);
+        // starter 1
+        let bowl1 = document.createElement("div");
+            bowl1.setAttribute("class", "menuItem");
+            bowl1.setAttribute("id", "bowl1");
+            let bowl1Title = document.createElement("h3");
+                bowl1Title.textContent = "Bowl 1"
+            let bowl1Price = document.createElement("span");
+                bowl1Price.textContent = "12";
+            let bowl1Text = document.createElement("p");
+                bowl1Text.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce iaculis tristique diam sed mattis. Nunc scelerisque mollis felis ac molestie.";
+            bowls.appendChild(bowl1);
+            bowl1.appendChild(bowl1Title);
+            bowl1.appendChild(bowl1Price);
+            bowl1.appendChild(bowl1Text);
+        // starter 2
+        let bowl2 = document.createElement("div");
+            bowl2.setAttribute("class", "menuItem");
+            bowl2.setAttribute("id", "bowl2");
+            let bowl2Title = document.createElement("h3");
+                bowl2Title.textContent = "Bowl 2"
+            let bowl2Price = document.createElement("span");
+                bowl2Price.textContent = "8";
+            let bowl2Text = document.createElement("p");
+                bowl2Text.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce iaculis tristique diam sed mattis. Nunc scelerisque mollis felis ac molestie.";
+            bowls.appendChild(bowl2);
+            bowl2.appendChild(bowl2Title);
+            bowl2.appendChild(bowl2Price);
+            bowl2.appendChild(bowl2Text);
+        // starter 3
+        let bowl3 = document.createElement("div");
+            bowl3.setAttribute("class", "menuItem");
+            bowl3.setAttribute("id", "bowl3");
+            let bowl3Title = document.createElement("h3");
+                bowl3Title.textContent = "Bowl 3"
+            let bowl3Price = document.createElement("span");
+                bowl3Price.textContent = "7";
+            let bowl3Text = document.createElement("p");
+                bowl3Text.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce iaculis tristique diam sed mattis. Nunc scelerisque mollis felis ac molestie.";
+            bowls.appendChild(bowl3);
+            bowl3.appendChild(bowl3Title);
+            bowl3.appendChild(bowl3Price);
+            bowl3.appendChild(bowl3Text);
         // maybe try a for each loop for menu items with an array?
 }
